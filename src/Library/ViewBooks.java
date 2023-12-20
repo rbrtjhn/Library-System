762cfae240d5ccce0b7bb3152dc525f070d52a3f
rbrtjhn <153427974+rbrtjhn@users.noreply.github.com>
2023-12-20 11:06:48 +0800

    ViewBooks.java

--- src/Library/ViewBooks.java
+++ src/Library/ViewBooks.java
@@ -26,7 +26,7 @@ public class ViewBooks implements IOOperation {
 		panel.setBorder(BorderFactory.createEmptyBorder(0, 30, 30, 30));
 		panel.setBackground(null);
 		
-		String[] row1 = new String[] {"Name", "Author", "Publisher", "Address", "Quantity", "Price", "Bor. Copies"};
+		String[] row1 = new String[] {"Name", "Author", "Publisher", "Quantity", "Price", "Bor. Copies"};
 		for (String s : row1) {
 			JLabel label = label(s);
 			panel.add(label);
@@ -36,14 +36,12 @@ public class ViewBooks implements IOOperation {
 			JLabel label1 = label(b.getName());
 			JLabel label2 = label(b.getAuthor());
 			JLabel label3 = label(b.getPublisher());
-			JLabel label4 = label(b.getAddress());
 			JLabel label5 = label(String.valueOf(b.getQuantity()));
 			JLabel label6 = label(String.valueOf(b.getPrice()));
 			JLabel label7 = label(String.valueOf(b.getBrwcopies()));
 			panel.add(label1);
 			panel.add(label2);
 			panel.add(label3);
-			panel.add(label4);
 			panel.add(label5);
 			panel.add(label6);
 			panel.add(label7);
